bf0c2dd8bcb198e762615524bf00ffa436f29087
blackshane <122633142+blackshane@users.noreply.github.com>
2023-02-22 15:35:45 -0600

    var assignments

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,8 @@
 // Assignment code here
-var lowerCharacters = [];
-var upperCharacters = [];
-var specialCharacters = [];
+var lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; 
+var specialCharacters = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "]", ";"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -17,3 +18,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+console.log(specialCharacters)
