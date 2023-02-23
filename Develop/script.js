da93e0ab8590163c91acaef5c78de32a35c6435d
blackshane <122633142+blackshane@users.noreply.github.com>
2023-02-23 17:27:26 -0600

    Added password min and max character

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,17 @@
 // Assignment code here
 var lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; 
+var numberCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; 
 var specialCharacters = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "]", ";"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
+document.getElementById("password").maxLength = "128"
+document.getElementById("password").minLength = "8"
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -18,5 +22,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-console.log(specialCharacters)
