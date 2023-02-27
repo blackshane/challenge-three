e50dad2849966b32dbf9e848ce8b0b80e3c458c6
blackshane <122633142+blackshane@users.noreply.github.com>
2023-02-27 14:32:07 -0600

    Better comments to work completed

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,5 @@
 // Assignment code here
+// Character possibilities
 const lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const numberCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; 
@@ -29,6 +30,7 @@ function generateString(length, characters) {
 
 function handleAcceptButtonClick() {
   // Verify 1 or more checkbox inputs are checked.
+  // checkbox inputs have a `.checked` attribute.
   let input_checked = inputSymbols.checked || inputNumbers.checked || inputLowers.checked || inputUppers.checked
   let inputLengthValue = inputLength.value
   if (!input_checked) {
